acf3b03b0bb6fa331d9221f5a6993a6c306152c2
MarinAndreGirard <marin.girard@outlook.com>
2024-02-25 00:50:48 +0100

    Made test file for new schmidt_solve functions. Note interesting graph of evolution of schmidt similarity with pas self.

--- Schmidt_solve.py
+++ Schmidt_solve.py
@@ -12,6 +12,7 @@ def compute_schmidt_states_new(result, time_index):
     eigenvalues, eigenstates = density_matrix.eigenstates() # Compute the eigenstates and eigenvalues of the density matrix
     eigenstates = [np.array(state) for state in eigenstates]
     schmidt_states_s = []
+    schmidt_states_e = []
     schmidt_values = []
     i=0
     for state, eigenvalue in zip(eigenstates, eigenvalues):
@@ -22,7 +23,7 @@ def compute_schmidt_states_new(result, time_index):
         else:
             #print(f"state {state}")
             i=i+1
-            N=abs(np.vdot(state.conjugate(),state))
+            N=abs(np.vdot(state,state))
             schmidt_states_s.append(state/np.sqrt(N)) # Normalize
 
     # Sort the Schmidt states by eigenvalue in descending order
@@ -33,18 +34,28 @@ def compute_schmidt_states_new(result, time_index):
     #compute the schmidt states of the environement.
     schmidt_states_e = []
     I = np.eye(d2)
+    #to get schmidt_env, we use that schmidt 1 and 2 of the system are |phi1> and |phi2>. 
+    #Then we have that the global state can always be written as |psi>=s1|phi1>|a1>+s2|phi2>|a2>  
     for j in range(i):
-        state = schmidt_states_s[j]
-        P_a_state = np.kron(np.outer(state,state.conjugate().T),I)     #def projector |><|xId, outer transposes teh second one
-        temp = np.dot(P_a_state,global_state) #vdot is conjugate on first one.
-        #print(np.sum(temp))
-        N = abs(np.vdot(global_state,temp))
-        #print(N)
-        schmidt_states_e.append(temp/np.sqrt(N))
-    #print(schmidt_values)
+        #step 1: get |phi_i>|ai>
+        state = schmidt_states_s[j] #getting |phi_i>, is normalized
+        P_a_state = np.kron(np.outer(state,state.conjugate().T),I) #def projector |phi_i><phi_i|xId, np.outer transposes the second one
+        temp = np.dot(P_a_state,global_state) #We apply the projector on the global state: P|psi>=s1|phi_i>|a_i> and normalize. vdot is conjugate on first one. To normalize /schmidt_values[j] potentially
+        temp = temp.flatten()
+        #step 2:We have |phi_i>|a_i>. We know |phi_i> and want |a_i>. |phi_i>|a_i> = (phi^1_i|a_i>,phi^2_i|a_i>, ...)
+        #find the first nonzero coeff of |phi_i> and use it to extract |a_i>
+        nonzero_index = np.nonzero(state)[0][0] #will need to know the index of a nonzero value in |phi_i>
+        #now extract a d2 sized vector from thatd2*d1 sized vector
+        temp2 = temp[nonzero_index*d2:(nonzero_index+1)*d2] #for k the nnzero index, this is phi^k_i|a_i>
+        temp3=temp2/state[nonzero_index]
+        N = abs(np.vdot(temp3,temp3))
+        
+        schmidt_states_e.append(temp3/np.sqrt(N))
+
     return schmidt_states_s,schmidt_states_e,schmidt_values
 
 
+#Note, outer(a,b) is (a1.b, a2.b,...) and |a>|b>=(a1|b>,a2|b>,...)
 def compute_schmidt_full(result,idx,s=1):
     ss, se, sv = compute_schmidt_states_new(result, idx)
     if s==1:
@@ -52,14 +63,14 @@ def compute_schmidt_full(result,idx,s=1):
         a = np.squeeze(a)
         b = se[0] #schmidt 1 on system 2
         b=np.squeeze(b)
-        g = np.outer(a, b).flatten()
+        g = np.outer(a,b).flatten()
         g=np.squeeze(g)
     elif s==2:
         a = ss[1] #schmidt 2 on system 1
         a = np.squeeze(a)
         b = se[1] #schmidt 2 on system 2
         b=np.squeeze(b)
-        g = np.outer(a, b).flatten()
+        g = np.outer(a,b).flatten()
         g=np.squeeze(g)
     return g
 
