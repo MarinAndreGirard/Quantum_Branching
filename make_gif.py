0fdfdd5c536bb1c8a0b4e43e058bd77021ba30a1
MarinAndreGirard <marin.girard@outlook.com>
2024-03-04 10:33:44 +0100

    Adding stuff to gifs

--- make_gif.py
+++ make_gif.py
@@ -113,13 +113,36 @@ def update_plot_new(frames,result,eigenstates_total,eigenenergies_total,EI,w,env
     state2 = compute_schmidt_full(result,frames,2)
     energy_coeff=[abs(np.vdot(state, eigenstate)) ** 2 for eigenstate in eigenstates_total]
     energy_coeff2=[abs(np.vdot(state2, eigenstate)) ** 2 for eigenstate in eigenstates_total]
-    plt.plot(eigenenergies_total,energy_coeff);
-    plt.plot(eigenenergies_total,energy_coeff2);
+    plt.plot(eigenenergies_total, energy_coeff)
+    plt.plot(eigenenergies_total, energy_coeff2)
     plt.title(f"Plot of the probability that Schmidt1 and 2 are in the energy eigenstates for EI={EI} and w={w} and env={env}")
     plt.xlabel("Eigenenergies of H_total")
     plt.ylabel("Probabilities")
-    plt.legend(["Schmidt1","Schmidt2"])
-    plt.ylim(0, 0.25)
+    plt.legend(["Schmidt1", "Schmidt2"])
+    plt.ylim(0, 0.35)
+    # Calculate the mean
+    mean1 = np.sum(np.array(energy_coeff) * np.array(eigenenergies_total))
+    mean2 = np.sum(np.array(energy_coeff2) * np.array(eigenenergies_total))
+    st1_tst1 = np.mean((np.array(energy_coeff) * np.array(eigenenergies_total)-mean1)**2)
+    st1_tst2 = np.mean((np.array(energy_coeff2) * np.array(eigenenergies_total)-mean2)**2)
+    std1 = np.std(np.array(energy_coeff) * np.array(eigenenergies_total))
+    std2 = np.std(np.array(energy_coeff2) * np.array(eigenenergies_total))
+    # Add a vertical line at the mean for energy_coeff
+    plt.axvline(x=mean1, color='b', linestyle='--')
+    # Add a vertical line at the mean for energy_coeff2
+    plt.axvline(x=mean2, color='r', linestyle='--')
+    # Add a vertical line at the mean plus one standard deviation for energy_coeff
+    plt.axvline(x=mean1 + st1_tst1, color='g', linestyle='--')
+    # Add a vertical line at the mean minus one standard deviation for energy_coeff
+    plt.axvline(x=mean1 - st1_tst1, color='g', linestyle='--')
+    # Add a vertical line at the mean plus one standard deviation for energy_coeff2
+    plt.axvline(x=mean2 + st1_tst2, color='c', linestyle='--')
+    # Add a vertical line at the mean minus one standard deviation for energy_coeff2
+    plt.axvline(x=mean2 - st1_tst2, color='c', linestyle='--')
+
+    #plt.legend()
+
+
 
 def make_gif_distribs1s2_new(EI,w,result,eigenstates_total,eigenenergies_total,env,d1,d2,E_spacing,tmax,ind_nb):
     print(env)
