a58293e8f890c51f0e78710be2f422e32b4cfd08
MarinAndreGirard <marin.girard@outlook.com>
2024-03-05 11:52:45 +0100

    had a tinker at overlap. found stupid mistake. made it sqrt, looked at it linearly.

--- overlap.py
+++ overlap.py
@@ -3,28 +3,30 @@ import qutip as qt
 import math
 import matplotlib.pyplot as plt
 from Schmidt_solve import compute_schmidt_states
+from Schmidt_solve import compute_schmidt_full
 
 #Defining the overlap of states in probability space
 # a measure of similarity of weights in the energie eigenbasis
 
 def get_p_s2(state,eigenstates_total):
-    p=[abs(np.vdot(state, eigenstate))  for eigenstate in eigenstates_total]
+    p=[abs(np.vdot(state, eigenstate)) ** 2  for eigenstate in eigenstates_total]
     return p
 
-def get_p(state,eigenstates_total):
-    p=[abs(np.vdot(state, eigenstate)) ** 2 for eigenstate in eigenstates_total]
-    return p
+#def get_p(state,eigenstates_total):
+#    p=[abs(np.vdot(state, eigenstate)) ** 2 for eigenstate in eigenstates_total]
+#    return p
 
-def get_p_2(state,eigenstates_total):
-    p=[(abs(np.vdot(state, eigenstate)) ** 2) **2 for eigenstate in eigenstates_total]
-    return p
+#def get_p_2(state,eigenstates_total):
+#    p=[(abs(np.vdot(state, eigenstate)) ** 2) **2 for eigenstate in eigenstates_total]
+#    return p
 
 def p_overlap(state1,state2,eigenstates_total):
-    p1 = get_p_s2(state1,eigenstates_total)
-    p2 = get_p_s2(state2,eigenstates_total)
-    overlap = np.vdot(p1, p2)
+    sqrt_p1 = np.sqrt(get_p_s2(state1,eigenstates_total))
+    sqrt_p2 = np.sqrt(get_p_s2(state2,eigenstates_total))
+    overlap = np.dot(sqrt_p1, sqrt_p2)
     return overlap
 
+'''
 def compute_schmidt_full(result, idx,s=1):
     if s==1:
         a = compute_schmidt_states(result, idx, 0, 0)[0] #schmidt 1 on system 1
@@ -41,26 +43,32 @@ def compute_schmidt_full(result, idx,s=1):
     else: 
         print("wrong input value")
     return g
+'''
 
-def plot_p_overlap_graph(tlist,result,H_total,w,EI):
+def plot_p_overlap_graph(tlist,result,H_total,w,EI,log_scale=False):
     o01 = []
     o02 = []
     o12 = []
     eigenenergies_total, eigenstates_total = H_total.eigenstates()
-    for idx in range(len(tlist)):
-        s1=compute_schmidt_full(result,idx,1)
-        s2=compute_schmidt_full(result,idx,2)
+    for idx in range(len(tlist)-2):
+        s1=compute_schmidt_full(result,idx+1,1)
+        s2=compute_schmidt_full(result,idx+1,2)
+        global_state = result.states[idx+1]
         #s3=compute_schmidt_full(result,idx,3)
-        o01.append(p_overlap(result.states[idx],s1,eigenstates_total))
-        o02.append(p_overlap(result.states[idx],s2,eigenstates_total))
+        o01.append(p_overlap(global_state,s1,eigenstates_total))
+        o02.append(p_overlap(global_state,s2,eigenstates_total))
         o12.append(p_overlap(s1,s2,eigenstates_total))
 
     plt.figure(figsize=(7, 4))
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.plot(o01)
-    plt.plot(o02)
-    plt.plot(o12)
+    if log_scale:
+        plt.xscale("log")
+        plt.yscale("log")
+    else:
+        plt.xscale("linear")
+        plt.yscale("linear")
+    plt.plot(tlist[0:len(tlist)-2],o01)
+    plt.plot(tlist[0:len(tlist)-2],o02)
+    plt.plot(tlist[0:len(tlist)-2],o12)
     plt.title("Graph of the overlap of global v schmidt 1 v schmidt 2")
     plt.xlabel("Time")
     plt.ylabel(r"$\sqrt{\vec{pi}} \cdot \sqrt{\vec{pj}}$")
